e6ba8475c0bd70f51ce9e47acc8e59628c4eccf9
Charles <charleslanop@gmail.com>
2023-06-25 19:16:09 -0300

    update openai

--- src/service/openaiService.ts
+++ src/service/openaiService.ts
@@ -13,17 +13,24 @@ export default class OpenaiService {
   public static async send(content: string, userId: string): Promise<IOpenai> {
     try {
       const response = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo-0301',
+        model: 'gpt-3.5-turbo',
         messages: [
+          {
+            role: 'system',
+            content:
+              'você é um analista de testes que trabalha para uma empresa de softwares',
+          },
           {
             role: 'user',
             content:
-              'crie até 3 casos de testes com passo a passo e resultado esperado para o requisito\nuse a tecnica de valores limitrofes. Por favor, forneça-nos um trecho da documentação do software ou um requisito específico que deseja validar.\n\nContexto:\nEx.: Sistema de cartão de credito de um banco\n\nEntrada: Ex.: o cartão de credito deve ser bloqueado 5 dias após o vencimento da fatura se o cliente não pagar\nSaída:',
+              'crie até 3 casos de testes com passo a passo e resultado esperado para o requisito\nuse a tecnica de valores limitrofes ou outras tecnicas que julgar necessario para validar o sistema. Por favor, forneça-nos um trecho da documentação do software ou um requisito específico que deseja validar.\n\nContexto:\nEx.: Sistema de cartão de credito de um banco\n\nEntrada: Ex.: o cartão de credito deve ser bloqueado 5 dias após o vencimento da fatura se o cliente não pagar\nSaída:',
           },
+          // {
+          //   role: 'user',
+          //   content:
+          //     'crie até 3 casos de testes com passo a passo e resultado esperado para o requisito\nuse a tecnica de valores limitrofes ou outras tecnicas que julgar necessario para validar o sistema. Por favor, forneça-nos um trecho da documentação do software ou um requisito específico que deseja validar.\n\nEntrada: Ex.: o cartão de credito deve ser bloqueado 5 dias após o vencimento da fatura se o cliente não pagar\nSaída:',
+          // },
         ],
-        max_tokens: 500,
-        n: 1,
-        temperature: 0.2,
       });
       const model = <UserHistoricModel>{};
       model.userId = userId;
