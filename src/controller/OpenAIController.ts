64f1120612de149d610610a1a0932ebd4b25dbe0
Charles <charleslanop@gmail.com>
2023-07-12 19:14:06 -0300

    update openai

--- src/controller/OpenAIController.ts
+++ src/controller/OpenAIController.ts
@@ -9,12 +9,12 @@ export default class OpenAIController {
     response: Response,
     next: NextFunction
   ) {
-    logger.info(`Send message from openai ${JSON.stringify(request.body)}`);
+    logger.info(`Send message from openAI ${JSON.stringify(request.body)}`);
     try {
-      const sendOpenai = request.body as ISendOpenAI;
+      const sendOpenAI = request.body as ISendOpenAI;
       return response
         .status(200)
-        .json(await OpenAIService.send(sendOpenai, request.user.id));
+        .json(await OpenAIService.send(sendOpenAI, request.user.id));
     } catch (error) {
       next(error);
     }
